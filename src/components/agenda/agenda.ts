8b059e3996f9d94adec25316f81d464207fa1007
Danilo Cecilia <dcecilia@msxint.com>
2018-02-15 17:21:22 -0200

    Agenda functionalities done

--- src/components/agenda/agenda.ts
+++ src/components/agenda/agenda.ts
@@ -2,6 +2,9 @@ import { NavController } from "ionic-angular/index";
 import { Component, ChangeDetectionStrategy, OnInit } from "@angular/core";
 import * as moment from "moment";
 import { AlertController } from "ionic-angular/components/alert/alert-controller";
+import { AgendaProvider } from "../../providers/agenda/agenda";
+import { ViewController } from "ionic-angular/navigation/view-controller";
+import { NavParams } from "ionic-angular/navigation/nav-params";
 /**
  * Generated class for the AgendaComponent component.
  *
@@ -13,9 +16,13 @@ import { AlertController } from "ionic-angular/components/alert/alert-controller
   templateUrl: "agenda.html"
 })
 export class AgendaComponent implements OnInit {
+  events: any;
   eventSource;
   viewTitle;
   isToday: boolean;
+  param:any;
+  subtitle:string;
+  description:string;
 
   calendar = {
     mode: "month",
@@ -50,31 +57,92 @@ export class AgendaComponent implements OnInit {
   };
 
   ngOnInit() {
-    this.loadEvents();
+    if(this.param.loadType === "general"){
+      this.loadEvents();
+      this.description = "What message here?";
+    }
+    else{
+      this.getEvents(1);
+      this.description = "Do you confirm your enrollment for this date event?";
+    }
   }
 
   constructor(
     private navController: NavController,
-    private alertCtrl: AlertController
-  ) {}
+    private alertCtrl: AlertController,
+    private agendaProvider: AgendaProvider,
+    public viewCtrl: ViewController,
+    public navParams: NavParams
+  ) {
+    this.param = this.navParams.data;
+  }
+
+  bindSubTitle(event){
+    let start = moment(event.startTime).format("LT");
+    let end = moment(event.endTime).format("LT");
+
+    return start + ` - ` + end + `<br>` +
+            `
+            Session: `      + event.TrainingCode + `<br> 
+            Seats Status: ` + event.SeatStatus   + `<br>
+            instructor: `   + event.Instructor   + `<br>
+            Location: `     + event.Location     + `<br>`
+  }
+
+  bindElements(response){
+    if (response) {
+      this.events = response;
+      
+      this.events.forEach(element => {
+        element.startTime = new Date(element.startTime);
+        element.endTime = new Date(element.endTime);
+      });
+
+      this.eventSource = this.events;
+    }
+  }
 
   loadEvents() {
-    debugger;
-    this.eventSource = this.createRandomEvents();
+    this.agendaProvider.loadAllEvents("", "").subscribe(res => {
+      this.bindElements(res);
+    });
+  }
+
+  getEvents(trainingId){
+    this.agendaProvider.getEvents(trainingId).subscribe(res => {
+      this.bindElements(res);
+    });
   }
 
   onViewTitleChanged(title) {
     this.viewTitle = title;
   }
 
-  onEventSelected(event) {
-    let start = moment(event.startTime).format("LLLLL");
-    let end = moment(event.endTime).format("LLLLL");
+  doConfirm(event) {
+    
     let alert = this.alertCtrl.create({
       title: "" + event.title,
-      subTitle: "From " + start + "</br>To: " + end,
-      buttons: ["OK"]
+      subTitle: this.bindSubTitle(event),
+      message: this.description,
+      buttons: [ {
+        text: 'No',
+        handler: () => {
+          console.log('No');
+        }
+      },
+      {
+        text: 'Yes',
+        handler: () => {
+          console.log('Yes');
+        }
+      }]
     });
+
+    alert.present();
+  }
+
+  onEventSelected(event) {
+    this.doConfirm(event);
 
     console.log(
       "Event selected:" +
@@ -106,72 +174,72 @@ export class AgendaComponent implements OnInit {
   }
 
   onCurrentDateChanged(event: Date) {
-    var today = new Date();
-    today.setHours(0, 0, 0, 0);
-    event.setHours(0, 0, 0, 0);
-    this.isToday = today.getTime() === event.getTime();
+    // var today = new Date();
+    // today.setHours(0, 0, 0, 0);
+    // event.setHours(0, 0, 0, 0);
+    // this.isToday = today.getTime() === event.getTime();
   }
 
-  createRandomEvents() {
-    var events = [];
-    for (var i = 0; i < 50; i += 1) {
-      var date = new Date();
-      var eventType = Math.floor(Math.random() * 2);
-      var startDay = Math.floor(Math.random() * 90) - 45;
-      var endDay = Math.floor(Math.random() * 2) + startDay;
-      var startTime;
-      var endTime;
-      if (eventType === 0) {
-        startTime = new Date(
-          Date.UTC(
-            date.getUTCFullYear(),
-            date.getUTCMonth(),
-            date.getUTCDate() + startDay
-          )
-        );
-        if (endDay === startDay) {
-          endDay += 1;
-        }
-        endTime = new Date(
-          Date.UTC(
-            date.getUTCFullYear(),
-            date.getUTCMonth(),
-            date.getUTCDate() + endDay
-          )
-        );
-        events.push({
-          title: "All Day - " + i,
-          startTime: startTime,
-          endTime: endTime,
-          allDay: true
-        });
-      } else {
-        var startMinute = Math.floor(Math.random() * 24 * 60);
-        var endMinute = Math.floor(Math.random() * 180) + startMinute;
-        startTime = new Date(
-          date.getFullYear(),
-          date.getMonth(),
-          date.getDate() + startDay,
-          0,
-          date.getMinutes() + startMinute
-        );
-        endTime = new Date(
-          date.getFullYear(),
-          date.getMonth(),
-          date.getDate() + endDay,
-          0,
-          date.getMinutes() + endMinute
-        );
-        events.push({
-          title: "Event - " + i,
-          startTime: startTime,
-          endTime: endTime,
-          allDay: false
-        });
-      }
-    }
-    return events;
-  }
+  // createRandomEvents() {
+  //   var events = [];
+  //   for (var i = 0; i < 50; i += 1) {
+  //     var date = new Date();
+  //     var eventType = Math.floor(Math.random() * 2);
+  //     var startDay = Math.floor(Math.random() * 90) - 45;
+  //     var endDay = Math.floor(Math.random() * 2) + startDay;
+  //     var startTime;
+  //     var endTime;
+  //     if (eventType === 0) {
+  //       startTime = new Date(
+  //         Date.UTC(
+  //           date.getUTCFullYear(),
+  //           date.getUTCMonth(),
+  //           date.getUTCDate() + startDay
+  //         )
+  //       );
+  //       if (endDay === startDay) {
+  //         endDay += 1;
+  //       }
+  //       endTime = new Date(
+  //         Date.UTC(
+  //           date.getUTCFullYear(),
+  //           date.getUTCMonth(),
+  //           date.getUTCDate() + endDay
+  //         )
+  //       );
+  //       events.push({
+  //         title: "All Day -Teste" + i,
+  //         startTime: startTime,
+  //         endTime: endTime,
+  //         allDay: true
+  //       });
+  //     } else {
+  //       var startMinute = Math.floor(Math.random() * 24 * 60);
+  //       var endMinute = Math.floor(Math.random() * 180) + startMinute;
+  //       startTime = new Date(
+  //         date.getFullYear(),
+  //         date.getMonth(),
+  //         date.getDate() + startDay,
+  //         0,
+  //         date.getMinutes() + startMinute
+  //       );
+  //       endTime = new Date(
+  //         date.getFullYear(),
+  //         date.getMonth(),
+  //         date.getDate() + endDay,
+  //         0,
+  //         date.getMinutes() + endMinute
+  //       );
+  //       events.push({
+  //         title: "Event - " + i,
+  //         startTime: startTime,
+  //         endTime: endTime,
+  //         allDay: false
+  //       });
+  //     }
+  //   }
+  //   return events;
+  // }
 
   onRangeChanged(ev) {
     console.log(
@@ -179,9 +247,13 @@ export class AgendaComponent implements OnInit {
     );
   }
 
-  markDisabled = (date: Date) => {
-    var current = new Date();
-    current.setHours(0, 0, 0);
-    return date < current;
-  };
+  // markDisabled = (date: Date) => {
+  //   var current = new Date();
+  //   current.setHours(0, 0, 0);
+  //   return date < current;
+  // };
+
+  dismiss(){
+    this.viewCtrl.dismiss();
+  }
 }
