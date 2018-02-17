b903dfd0d22c31006848855e228180061ae308ca
Danilo Cecilia <danilo.cecilia@gmail.com>
2018-02-17 15:32:52 -0200

    implementing dashboard screen

--- src/components/dashboard/dashboard.ts
+++ src/components/dashboard/dashboard.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild } from "@angular/core";
 import chartJs from 'chart.js';
+import datalabels from 'chartjs-plugin-datalabels'
 
 /**
  * Generated class for the DashboardComponent component.
@@ -13,14 +14,18 @@ import chartJs from 'chart.js';
 })
 export class DashboardComponent {
   @ViewChild("lineCanvas") lineCanvas;
+  @ViewChild("barCanvas") barCanvas;
+  @ViewChild('pieCanvas') pieCanvas;
 
   lineChart: any;
+  barChart: any;
+  pieChart: any;
 
-  constructor() {}
+  constructor() { }
 
   ngAfterViewInit() {
     setTimeout(() => {
-      // this.barChart = this.getBarChart();
+      this.barChart = this.getBarChart();
       // this.doughnutChart = this.getDoughnutChart();
       // this.halfDoughnutChart = this.getHalfDoughnutChart();
     }, 150);
@@ -32,69 +37,123 @@ export class DashboardComponent {
     setTimeout(() => {
       // this.bubbleChart = this.getBubbleChart();
       // this.mixedChart = this.getMixedChart();
-      // this.pieChart = this.getPieChart();
+      this.pieChart = this.getPieChart();
     }, 350);
   }
 
   getLineChart() {
     const data = {
-      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      labels: [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20],
       datasets: [
         {
-          label: "My First dataset",
-          fill: false,
+          label: "Qty Access",
+          fill: true,
+          // yAxisID: "Qtd Access",
           lineTension: 0.1,
-          backgroundColor: "rgba(75,192,192,0.4)",
-          borderColor: "rgba(75,192,192,1)",
+          backgroundColor: "rgba(0,0,0,0.7)",
+          borderColor: "rgba(0,0,0,0.7)",
           borderCapStyle: "butt",
           borderDash: [],
           borderDashOffset: 0.0,
-          borderJoinStyle: "miter",
-          pointBorderColor: "rgba(75,192,192,1)",
+          borderJoinStyle: "bevel",
+          pointBorderColor: "rgba(0,0,0,0.7)",
           pointBackgroundColor: "#fff",
           pointBorderWidth: 1,
           pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgba(75,192,192,1)",
-          pointHoverBorderColor: "rgba(220,220,220,1)",
+          pointHoverBackgroundColor: "rgba(0,0,0,0.7)",
+          pointHoverBorderColor: "rgba(255,0,0,0.7)",
           pointHoverBorderWidth: 2,
           pointRadius: 1,
           pointHitRadius: 10,
-          data: [65, 59, 80, 81, 56, 55, 40],
-          spanGaps: false
-        },
-        {
-          label: "My Second dataset",
-          fill: false,
-          lineTension: 0.1,
-          backgroundColor: "rgba(175,92,192,0.4)",
-          borderColor: "rgba(31,156,156,1)",
-          borderCapStyle: "butt",
-          borderDash: [5, 8],
-          borderDashOffset: 0.0,
-          borderJoinStyle: "miter",
-          pointBorderColor: "rgba(31,156,156,1)",
-          pointBackgroundColor: "#fff",
-          pointBorderWidth: 1,
-          pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgba(31,156,156,1)",
-          pointHoverBorderColor: "rgba(220,220,220,1)",
-          pointHoverBorderWidth: 2,
-          pointRadius: 1,
-          pointHitRadius: 10,
-          data: [15, 39, 50, 81, 51, 55, 30],
+          data: [0, 2, 20, 6, 15, 55, 35, 2, 8, 1, 1],
           spanGaps: false
         }
       ]
     };
 
-    return this.getChart(this.lineCanvas.nativeElement, "line", data);
+    const options = {
+      scales: {
+        yAxes: [{
+          ticks: {
+            min: 0,
+            stepSize: 10,
+            max: 60
+          }
+        }]
+      }
+    };
+
+    return this.getChart(this.lineCanvas.nativeElement, "line", data, options);
   }
 
-  getChart(context, chartType, data, options?) {
-    return new chartJs(context, {
-      data,
-      options,
-      type: chartType,
-    });
+  getBarChart() {
+    const data = {
+      labels: ['Air', 'Food', 'Gifts', 'Location', 'Other', 'Training', 'Transport'],
+      datasets: [{
+        //label: '# of Votes',
+        data: [825.00, 308.70, 100.00, 499.00, 339.00, 45.00, 64.25],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)',
+          'rgba(255, 220, 60, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)',
+          'rgba(255, 220, 60, 1)'
+        ],
+        borderWidth: 1,
+        fill: false
+      }]
+    };
+
+    const options = {
+      legend: {
+        display: false
+      },
+      scales: {
+        yAxes: [{
+          gridLines: {
+            display: false,
+            drawBorder: false
+          },
+          ticks: {
+            min: 0,
+            stepSize: 300,
+            max: 850
+          }
+        }]
+      },
+      plugins: [datalabels]
+    };
+
+    return this.getChart(this.barCanvas.nativeElement, 'bar', data, options);
+  }
+
+  getPieChart() {
+    const data = {
+      labels: ['Enrolled', 'Fail', 'In Progress', 'Pass'],
+      labelFontSize : '6',
+      datasets: [
+        {
+          data: [226, 2467, 2787, 6000],
+          backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#64f28a'],
+          hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#64f28a']
+        }]
+    };
+
+    return this.getChart(this.pieCanvas.nativeElement, 'pie', data);
+  }
+
+  getChart(context, chartType, data, options?, plugins?) {
+    return new chartJs(context, { type: chartType, data, options, plugins });
   }
 }
