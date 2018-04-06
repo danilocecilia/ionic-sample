6c4ea938c949072c43ee4352dddb8e191956b8a3
danilocecilia <danilo.cecilia@gmail.com>
2018-04-06 17:34:47 -0300

    NotificationProvider: include new function to update read notification

--- src/providers/notification/notification.ts
+++ src/providers/notification/notification.ts
@@ -8,21 +8,25 @@ export class NotificationProvider {
   private cfg: any;
   notification: any;
 
-  constructor(public http: HttpClient, private auth: AuthProvider) {
+  constructor(public http: HttpClient, private authProvider: AuthProvider) {
     this.cfg = AppConfig.cfg;
   }
 
   loadNotifications() {
     return this.http
-      .get(
-        `${this.cfg.apiUrl + this.cfg.notification.all}?token=${
-          this.auth.loggedUser.Token
-        }`
-      )
+      .get(`${this.cfg.apiUrl + this.cfg.notification.all}?token=${this.authProvider.loggedUser.Token}`)
       .toPromise();
       // .then(result => {
       //   debugger;
       //   this.notification = result;
       // });
+  }
+
+  setNotificationRead(id){
+    let notificationReadParams = {
+      ID: id,
+      Token: this.authProvider.loggedUser.Token
+    } 
+    return this.http.post(`${this.cfg.apiUrl + this.cfg.notification.notifyRead}`, notificationReadParams).toPromise();
   }
 }
