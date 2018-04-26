44815fcd7297cf71e37be43fbc7adf3cb7d23916
Danilo Cecilia <danilo.cecilia@gmail.com>
2018-04-26 20:31:41 -0300

    Download Provider: refactor code

--- src/providers/donwload/donwload.ts
+++ src/providers/donwload/donwload.ts
@@ -4,6 +4,7 @@ import { Platform } from "ionic-angular";
 import { File } from "@ionic-native/file";
 import { FileTransferObject, FileTransfer } from "@ionic-native/file-transfer";
 import * as AppConfig from "../../app/config";
+import { AndroidPermissions } from "@ionic-native/android-permissions";
 
 @Injectable()
 export class DonwloadProvider {
@@ -13,27 +14,41 @@ export class DonwloadProvider {
     private file: File,
     private platform: Platform,
     private fileOpener: FileOpener,
-    private transfer: FileTransfer
+    private transfer: FileTransfer,
+    private androidPermissions: AndroidPermissions
   ) {}
 
   initializeFileObject(filePath, fileName, extension) {
     this.fileObject = this.getFileObject(filePath, fileName, extension);
   }
 
-  openFile(checkFileOnDevice: boolean) {
-    if (checkFileOnDevice) {
-      return this.getFileFromDevice()
-        .then(resolve => {
-          this.openDocument();
-        })
-        .catch(() => {
-          //If the file was not found on the device, then I should download it from Source.
-          this.donwloadFile();
-          return "downloaded";
-        });
+  /**
+   * Check if a file exists in the localstorage of the device.
+   *
+   * @param {string} checkOnDeviceFirst If this flag is true It will check if the file exists in a device before download it.
+   * @returns {Promise<boolean>} Returns a "downloaded" or "opened"text that will be used to specify a toast message for the user.
+   */
+  openFile(checkOnDeviceFirst: boolean) {
+    if (checkOnDeviceFirst) {
+      return this.file.checkFile(this.fileObject.targetPath, this.fileObject.targeFileName)
+      .then(() => {
+        this.openDocument();  
+        return "openened";
+      }).catch((err) => {
+        return this.donwloadFile();  
+      }).then((msg) => {return msg});
     } else {
-      return this.donwloadFile();
+      return this.donwloadFile();  
     }
+  }
+
+  checkAndroidPermission(){
+    this.androidPermissions.checkPermission(this.androidPermissions.PERMISSION.WRITE_EXTERNAL_STORAGE).then(
+      result => console.log('Has permission?',result.hasPermission),
+      err => this.androidPermissions.requestPermission(this.androidPermissions.PERMISSION.WRITE_EXTERNAL_STORAGE)
+    );
+    
+    this.androidPermissions.requestPermissions([this.androidPermissions.PERMISSION.WRITE_EXTERNAL_STORAGE, this.androidPermissions.PERMISSION.GET_ACCOUNTS]);
   }
 
   openDocument() {
@@ -45,17 +60,21 @@ export class DonwloadProvider {
   }
 
   donwloadFile() {
+    this.checkAndroidPermission();
+
     if (this.fileObject) {
       const fileTransfer: FileTransferObject = this.transfer.create();
 
-      const fullSourcePath = `${AppConfig.cfg.baseUrl}${
-        this.fileObject.sourceFilePath
-      }/${this.fileObject.sourceFileName}`; //"http://198.180.251.216:10002/Temp/Library/Bobber_DSG_EN.pdf";
+      const fullSourcePath = `${AppConfig.cfg.baseUrl}${this.fileObject.sourceFilePath}/${this.fileObject.sourceFileName}`; //"http://198.180.251.216:10002/Temp/Library/Bobber_DSG_EN.pdf";
 
-      return fileTransfer.download(
+      let fileEntry = fileTransfer.download(
         fullSourcePath,
         this.getDocumentPathFromDevice(this.fileObject.sourceFileName)
       );
+
+      if(fileEntry){
+        return Promise.resolve("downloaded");
+      }
     }
   }
 
@@ -68,14 +87,7 @@ export class DonwloadProvider {
   }
 
   getFileFromDevice() {
-    const promise = new Promise((resolve, reject) => {
-      this.file
-        .checkFile(this.fileObject.targetPath, this.fileObject.targeFileName)
-        .then(() => resolve())
-        .catch(() => reject());
-    });
-
-    return promise;
+      return 
   }
 
   getFileObject(filePath: string, fileName: string, extension: string) {
