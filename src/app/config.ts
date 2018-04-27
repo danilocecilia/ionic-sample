29f4f3c98580731c6103b9cc2dfacc89e091f022
danilocecilia <danilo.cecilia@gmail.com>
2018-04-27 17:01:00 -0300

    general changes

--- src/app/config.ts
+++ src/app/config.ts
@@ -4,17 +4,20 @@
 //let baseUrl = "http://136.140.193.16:10002/API/Authentication/";
 
 export let cfg = {
-  // baseUrl: "http://136.140.193.16:10003",      // EXTERNAL IP
-  // apiUrl: "http://136.140.193.16:10003/API/",  // INTERNAL IP
-  baseUrl: "http://198.180.251.216:10003",
-  apiUrl: "http://198.180.251.216:10003/API/", 
+  baseUrl: "http://136.140.193.16:10003", // EXTERNAL IP
+  apiUrl: "http://136.140.193.16:10003/API/", // INTERNAL IP
+  // baseUrl: "http://198.180.251.216:10003",
+  // apiUrl: "http://198.180.251.216:10003/API/",
   tokenName: "token",
   agenda: {
     getClassesByDate: "Class/GetClassesByDate",
     getClassesByTraining: "Class/GetClassesByTraining"
   },
   event_summary: {
-    getEventSummary: "Class/GetEventSummary"
+    getEventSummary: "Class/GetEventSummary",
+    putActionStep: "Class/ActionStep",
+    putActionStatus: "Class/ActionStatus",
+    enrollmentsByClass: "History/GetEnrollmentsByClass"
   },
   user: {
     register: "",
@@ -43,6 +46,19 @@ export let cfg = {
     all: "Media/GetMedias"
   }
 };
+
+export enum HistoryStatus {
+  NOT_STARTED,
+  IN_PROGRESS,
+  FINISHED,
+  CANCELLED
+}
+
+export enum TrainingSteps{
+  PRE_TEST, 
+  TRAINING, 
+  POST_TEST,
+}
 
 export enum APIPermission {
   CURRICULUM,
@@ -74,19 +90,30 @@ export enum APIStatus {
 }
 
 export let fileMimeTypes = [
-  { name : "PDF",  type: "application/pdf" }, 
-  { name : "DOC",  type: "application/msword"},
-  { name : "DOCX", type: "application/vnd.openxmlformats-officedocument.wordprocessingml.document"},
-  { name : "XLS",  type: "application/vnd.ms-excel" },
-  { name : "XLSX", type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" },
-  { name : "PPT",  type: "application/vnd.ms-powerpoint"},
-  { name : "PPTX", type: "application/vnd.openxmlformats-officedocument.presentationml.presentation"},
-  { name : "MP4A", type: "audio/mp4"},
-  { name : "MP4",  type: "application/mp4"},
-  { name : "AVI",  type: "video/x-msvideo"},
-  { name : "PNG",  type: "image/png"},
-  { name : "JPG",  type: "image/jpeg"},
-  { name : "JPEG", type: "image/jpeg"}
+  { name: "PDF", type: "application/pdf" },
+  { name: "DOC", type: "application/msword" },
+  {
+    name: "DOCX",
+    type:
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+  },
+  { name: "XLS", type: "application/vnd.ms-excel" },
+  {
+    name: "XLSX",
+    type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+  },
+  { name: "PPT", type: "application/vnd.ms-powerpoint" },
+  {
+    name: "PPTX",
+    type:
+      "application/vnd.openxmlformats-officedocument.presentationml.presentation"
+  },
+  { name: "MP4A", type: "audio/mp4" },
+  { name: "MP4", type: "application/mp4" },
+  { name: "AVI", type: "video/x-msvideo" },
+  { name: "PNG", type: "image/png" },
+  { name: "JPG", type: "image/jpeg" },
+  { name: "JPEG", type: "image/jpeg" }
 ];
 
 export let cultures = ["pt-BR", "en-US", "es"];
