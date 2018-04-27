671435d8d6ce5027840a327b3fd90d0fbdc912b9
danilocecilia <danilo.cecilia@gmail.com>
2018-04-27 16:58:25 -0300

    EventSummary: get data dinamically

---
+++ src/model/event-sumary-actions.ts
@@ -0,0 +1,9 @@
+export class EventSummaryActionStatus {
+  IdClass: number;
+  Status: number;
+}
+
+export class EventSummaryActionStep {
+    IdClass: number;
+    Status: number;
+}
--- src/providers/event-summary/event-summary.ts
+++ src/providers/event-summary/event-summary.ts
@@ -2,13 +2,21 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import * as APPConfig from "../../app/config";
 import { AuthProvider } from '../auth/auth';
-
+import { EventSummaryActionStatus, EventSummaryActionStep } from "../../model/event-sumary-actions";
 @Injectable()
 export class EventSummaryProvider {
   constructor(public http: HttpClient, private authProvider : AuthProvider) {
   }
 
-  getEventSummary(idClass:number){
+  getEventSummaryByClass(idClass:number){
     return this.http.get(`${APPConfig.cfg.apiUrl}${APPConfig.cfg.event_summary.getEventSummary}?token=${this.authProvider.loggedUser.Token}&idClass=${idClass}`).toPromise();
+  }
+
+  updateActionStep(step : EventSummaryActionStep){
+    return this.http.post(`${APPConfig.cfg.apiUrl}${APPConfig.cfg.event_summary.putActionStep}?token=${this.authProvider.loggedUser.Token}`, step).toPromise();
+  }
+
+  updateActionStatus(status : EventSummaryActionStatus){
+    return this.http.post(`${APPConfig.cfg.apiUrl}${APPConfig.cfg.event_summary.putActionStatus}?token=${this.authProvider.loggedUser.Token}`, status).toPromise();
   }
 }
