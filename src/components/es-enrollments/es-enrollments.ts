57565d7cf309d132d2b18b4e19415ef3aa8a9c9e
danilocecilia <danilo.cecilia@gmail.com>
2018-05-03 16:23:56 -0300

    Enrollments: get data from api

--- src/components/es-enrollments/es-enrollments.ts
+++ src/components/es-enrollments/es-enrollments.ts
@@ -1,8 +1,13 @@
 import { Component, OnInit } from "@angular/core";
 import { NavController, NavParams } from "ionic-angular";
 import { EsEnrollComponent } from "../es-enroll/es-enroll";
-import { EnrollmentProvider } from "../../providers/enrollment/enrollment";
 import { LoadingProvider } from "../../providers/loading/loading";
+import * as AppConfig from "../../app/config";
+import { THIS_EXPR } from "@angular/compiler/src/output/output_ast";
+import { EnrollmentProvider } from "../../providers/enrollment/enrollment";
+import { GradeHistory } from "../../model/enrollments";
+import { ToastProvider } from "../../providers/toast/toast";
+
 
 @Component({
   selector: "es-enrollments",
@@ -10,22 +15,40 @@ import { LoadingProvider } from "../../providers/loading/loading";
 })
 export class EsEnrollmentsComponent implements OnInit {
   enrollments: any = {};
-  
+  baseUrl = AppConfig.cfg.baseUrl;
+  enrollment: any;
+
   constructor(
     private navCtrl: NavController,
-    private enrollmentProvider: EnrollmentProvider,
     private loadingProvider: LoadingProvider,
-    private navParams: NavParams
+    private navParams: NavParams,
+    private enrollmentProvider: EnrollmentProvider,
+    private toastProvider: ToastProvider
   ) {
     this.enrollments = this.navParams.get("enrollments");
-    debugger;
   }
 
-  ngOnInit() {
-    this.loadingProvider.presentLoadingDefault();
+  ngOnInit() {}
+
+  remove(event) {
+    this.enrollment = {
+      ID_History: event.ID
+    };
+    
+    this.enrollmentProvider
+      .removeEnrollment(this.enrollment)
+      .then(status => {
+        if(status === "SUCCESS"){
+          this.enrollments.Histories = this.enrollments.Histories.filter(element => element.ID !== event.ID);
+          this.toastProvider.presentTranslatedToast("SuccessRemovalEnrollment");
+        }
+      })
+      .catch(err => {
+        this.toastProvider.presentTranslatedToast("ErrorMessage");
+      });
   }
 
   onClickEnrollUsers() {
-    this.navCtrl.push(EsEnrollComponent, { event : this.enrollments});
+    this.navCtrl.push(EsEnrollComponent, { event: this.enrollments });
   }
 }
