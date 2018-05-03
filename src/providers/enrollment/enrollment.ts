e17504f45efb5e6e11ee8ae81bd2be520c5db124
danilocecilia <danilo.cecilia@gmail.com>
2018-05-03 12:08:00 -0300

    EnrollmentProvider: call new api to update grade history

--- src/providers/enrollment/enrollment.ts
+++ src/providers/enrollment/enrollment.ts
@@ -2,17 +2,22 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import * as APPConfig from "../../app/config";
 import { AuthProvider } from '../auth/auth';
+import { GradeHistory } from "../../model/enrollments";
 
 @Injectable()
 export class EnrollmentProvider {
   constructor(public http: HttpClient, private authProvider : AuthProvider) {
   }
 
-  // loadEnrollments() {
-  //   return this.http.get(`${this.baseUrl}/enrollments/`);
-  // }
-
   loadEnrollmentsByClass(idClass:number){
-    return this.http.get(`${APPConfig.cfg.apiUrl}${APPConfig.cfg.event_summary.enrollmentsByClass}?token=${this.authProvider.loggedUser.Token}&idClass=${idClass}`).toPromise();
+    return this.http.get(`${APPConfig.cfg.apiUrl}${APPConfig.cfg.history.enrollmentsByClass}?token=${this.authProvider.loggedUser.Token}&idClass=${idClass}`).toPromise();
+  }
+
+  removeEnrollment(enrollment: GradeHistory){
+    return this.http.post(`${APPConfig.cfg.apiUrl}${APPConfig.cfg.history.removeEnrollment}?token=${this.authProvider.loggedUser.Token}`, enrollment).toPromise();
+  }
+  
+  updateGrades(gradeHistory : GradeHistory){
+    return this.http.post(`${APPConfig.cfg.apiUrl}${APPConfig.cfg.history.updateGrade}?token=${this.authProvider.loggedUser.Token}`, gradeHistory).toPromise();
   }
 }
