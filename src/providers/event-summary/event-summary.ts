b7618722bfbeeada23ac1354928063e7030fa900
Danilo Cecilia <danilo.cecilia@gmail.com>
2018-05-03 21:10:24 -0300

    Add new function

--- src/providers/event-summary/event-summary.ts
+++ src/providers/event-summary/event-summary.ts
@@ -1,22 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import * as APPConfig from "../../app/config";
+import * as AppConfig from "../../app/config";
 import { AuthProvider } from '../auth/auth';
 import { EventSummaryActionStatus, EventSummaryActionStep } from "../../model/event-sumary-actions";
 @Injectable()
 export class EventSummaryProvider {
-  constructor(public http: HttpClient, private authProvider : AuthProvider) {
+  constructor(private http: HttpClient, private authProvider : AuthProvider) {
   }
 
   getEventSummaryByClass(idClass:number){
-    return this.http.get(`${APPConfig.cfg.apiUrl}${APPConfig.cfg.event_summary.getEventSummary}?token=${this.authProvider.loggedUser.Token}&idClass=${idClass}`).toPromise();
+    return this.http.get(`${AppConfig.cfg.apiUrl}${AppConfig.cfg.event_summary.getEventSummary}?token=${this.authProvider.loggedUser.Token}&idClass=${idClass}`).toPromise();
   }
 
   updateActionStep(step : EventSummaryActionStep){
-    return this.http.post(`${APPConfig.cfg.apiUrl}${APPConfig.cfg.event_summary.putActionStep}?token=${this.authProvider.loggedUser.Token}`, step).toPromise();
+    return this.http.post(`${AppConfig.cfg.apiUrl}${AppConfig.cfg.event_summary.putActionStep}?token=${this.authProvider.loggedUser.Token}`, step).toPromise();
   }
 
   updateActionStatus(status : EventSummaryActionStatus){
-    return this.http.post(`${APPConfig.cfg.apiUrl}${APPConfig.cfg.event_summary.putActionStatus}?token=${this.authProvider.loggedUser.Token}`, status).toPromise();
+    return this.http.post(`${AppConfig.cfg.apiUrl}${AppConfig.cfg.event_summary.putActionStatus}?token=${this.authProvider.loggedUser.Token}`, status).toPromise();
+  }
+
+  getClassById(idClass){
+    return this.http.get(`${AppConfig.cfg.apiUrl}${AppConfig.cfg.event_summary.getClass}?token=${this.authProvider.loggedUser.Token}&idClass=${idClass}`).toPromise();
   }
 }
