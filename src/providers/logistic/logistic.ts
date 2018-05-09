c4db0aac46bc49da5f32a1eb6479e3cc8100d1e0
danilocecilia <danilo.cecilia@gmail.com>
2018-05-09 16:56:47 -0300

    Logistic: new functions

--- src/providers/logistic/logistic.ts
+++ src/providers/logistic/logistic.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import * as AppConfig from "../../app/config";
 import { AuthProvider  } from "../../providers/auth/auth";
 import { App } from 'ionic-angular';
+import { Logistic } from '../../model/logistic';
 @Injectable()
 export class LogisticProvider {
 
@@ -26,4 +27,15 @@ export class LogisticProvider {
     return this.http.post(`${AppConfig.cfg.apiUrl}${AppConfig.cfg.logistic.remove}?token=${this.authProvider.loggedUser.Token}`, idLogisticItem).toPromise();
   }
 
+  getFilesByLogistic(idLogistic){
+    return this.http.get(`${AppConfig.cfg.apiUrl}${AppConfig.cfg.logistic.getFiles}?token=${this.authProvider.loggedUser.Token}&ID_LogisticItemXClass=${idLogistic}`).toPromise();
+  }
+
+  removeFile(idFile, idLogistic){
+    return this.http.post(`${AppConfig.cfg.apiUrl}${AppConfig.cfg.logistic.removeFile}?ID_LogisticItemXClass=${idLogistic}&token=${this.authProvider.loggedUser.Token}`, idFile).toPromise();
+  }
+
+  updateLogistic(obj:Logistic){
+    return this.http.post(`${AppConfig.cfg.apiUrl}${AppConfig.cfg.logistic.update}?token=${this.authProvider.loggedUser.Token}`, obj).toPromise();
+  }
 }
