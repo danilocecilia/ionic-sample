a790cecd47920f5990b9541b36852c4dcc1b37d3
danilocecilia <danilo.cecilia@gmail.com>
2018-05-25 15:49:15 -0300

    refactored app componentes using userStore observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,53 +1,64 @@
-import { Component, ViewChild } from "@angular/core";
-import { Platform, NavController, AlertController } from "ionic-angular";
+import { Component, ViewChild, ChangeDetectionStrategy } from "@angular/core";
+import {
+  Platform,
+  NavController,
+  AlertController
+} from "ionic-angular";
 import { StatusBar } from "@ionic-native/status-bar";
 import { SplashScreen } from "@ionic-native/splash-screen";
-import { Events } from "ionic-angular";
+import { TranslateService } from "@ngx-translate/core";
+import { Globalization } from "@ionic-native/globalization";
+import { PushOptions, PushObject, Push } from "@ionic-native/push";
+import * as AppConfig from "../app/config";
+
 import { AuthProvider } from "../providers/auth/auth";
 import { TabsPage } from "../pages/tabs/tabs";
 import { AuthPage } from "../pages/auth/auth";
-import { TranslateService } from "@ngx-translate/core";
-import { Globalization } from "@ionic-native/globalization";
-import { timer  } from "rxjs/observable/timer";
-import { PushOptions, PushObject, Push } from "@ionic-native/push";
+import { UserStore } from "../stores/user.store";
 
 @Component({
   templateUrl: "app.html"
 })
 export class MyApp {
   currentUser: any;
-  @ViewChild("content") nav: NavController;
   showSplash = true;
+  @ViewChild("content") nav: NavController;
+
   constructor(
-    platform: Platform,
-    statusBar: StatusBar,
-    splashScreen: SplashScreen,
-    private events: Events,
+    private platform: Platform,
+    private statusBar: StatusBar,
+    private splashScreen: SplashScreen,
     private authProvider: AuthProvider,
     private translate: TranslateService,
     private globalization: Globalization,
     private push: Push,
-    private alertCtrl: AlertController
-    
+    private alertCtrl: AlertController,
+    private userStore: UserStore
   ) {
     platform.ready().then(() => {
-      statusBar.backgroundColorByHexString("#003a8b");
-      splashScreen.hide();
-      
-      this.pushsetup();
-
-      translate.addLangs(["en", "pt"]);
-
-      this.getLocalStorageUser()
-        .then(() => this.bindCulture())
-        .then(() => this.checkIfUserIsLogged())
-        .then(() => this.authProvider.startupTokenRefresh())
+      this.initializeAplication(statusBar, splashScreen);
     });
-
-    timer(3000).subscribe(() => this.showSplash = false);
   }
 
-  pushsetup() {
+  initializeAplication(statusBar, splashScreen){
+    statusBar.backgroundColorByHexString(AppConfig.COLORS.darkblue);
+
+    splashScreen.hide();
+
+    this.currentUser = this.userStore.user;
+
+    this.authProvider.startupTokenRefresh();
+
+    this.setupPushNotification();
+
+    this.loadDefaultLanguage();
+
+    this.redirectToPage();
+
+    this.showSplash = false;
+  }
+
+  setupPushNotification() {
     const options: PushOptions = {};
 
     const pushObject: PushObject = this.push.init(options);
@@ -65,51 +76,46 @@ export class MyApp {
     });
   }
 
-  checkIfUserIsLogged() {
-    if (!this.currentUser) this.nav.setRoot(AuthPage);
-    else {
+  redirectToPage() {
+    if (this.currentUser) {
       this.nav.setRoot(TabsPage);
+    } else {
+      this.nav.setRoot(AuthPage);
     }
   }
 
-  getLocalStorageUser() {
-    return this.authProvider.getLoggedUser().then(res => {
-      this.currentUser = res;
-
-      this.events.publish("currentUser", this.currentUser);
-    });
-  }
-
-  bindCulture() {
+  loadDefaultLanguage() {
     let user = this.currentUser;
 
     if (user) {
       if (user.Language && user.Language.Culture) {
-        let culture = user.Language.Culture.substring(0, 2);
+        let prefix = user.Language.Culture.substring(0, 2);
 
-        this.setDefaultCulture(culture);
+        this.setLanguage(prefix);
       } else {
-        this.setDefaultCulture("en");
+        this.setLanguage(AppConfig.CULTURES.english);
       }
     } else {
-      this.globalization
-        .getPreferredLanguage()
-        .then(lang => {
-          if (lang) {
-            let c = lang.value.substring(0, 2);
-            this.setDefaultCulture(c);
-          }
-        })
-        .catch(e => {
-          this.setDefaultCulture("en");
-          console.error(e);
-        });
+      this.setDefaultLanguage();
     }
   }
 
-  
+  setDefaultLanguage() {
+    this.globalization
+      .getPreferredLanguage()
+      .then(language => {
+        if (language) {
+          let prefix = language.value.substring(0, 2);
+          this.setLanguage(prefix);
+        }
+      })
+      .catch(e => {
+        this.setLanguage(AppConfig.CULTURES.english);
+        console.error("Error setting default language: " + e);
+      });
+  }
 
-  setDefaultCulture(culture: string) {
+  setLanguage(culture: string) {
     this.translate.setDefaultLang(culture);
     this.translate.use(culture);
   }
